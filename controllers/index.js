55c5e049f44f839cdb1cce36f5a6a7bd1cfcd812
kevmhughes <kevhughes24@hotmail.com>
2024-09-02 18:14:19 +0200

    Modify getApartments controller, and create template engine in home.ejs view to be able to render all apartments in the database

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
-const getApartments = (req, res) => {
-    res.render("home.ejs")
+const Apartment = require('../models/apartment.model.js');
+
+const getApartments = async (req, res) => {
+    const apartments = await Apartment.find();
+
+    res.render("home", {
+        apartments
+    })
 }
 
 module.exports = {
