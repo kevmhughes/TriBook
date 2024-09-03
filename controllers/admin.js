b2678e2420649749817224f542336ee5ca5f4b41
kevmhughes <kevhughes24@hotmail.com>
2024-09-03 13:25:45 +0200

    Create controller and route, and modify "new apartment" form to add "edit apartment" functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,26 +1,56 @@
-const Apartment = require("../models/apartment.model.js")
+const Apartment = require("../models/apartment.model.js");
 
 const getNewApartmentForm = (req, res) => {
-    res.render("new-apartment.ejs")
-}
+  res.render("new-apartment.ejs", {
+    // send empty apartment object
+    apartment: {},
+  });
+};
+
+const getEditApartmentForm = async (req, res) => {
+  const { idApartment } = req.params;
+  const apartment = await Apartment.findById(idApartment);
+
+  res.render("new-apartment.ejs", {
+    // send apartment object to fill edit form with apartment data
+    apartment,
+  });
+};
 
 const postNewApartment = async (req, res) => {
+    // "id" from name="id" in the HTML form
+  const { id } = req.body;
 
-    console.log("console log", req.body)
-    
-    await Apartment.create({
-        title: req.body.title,
-        description: req.body.description,
-        price: req.body.price,
-        size: req.body.size,
-        mainPhoto: req.body.mainPhoto
+    // it is possible to send simply req.body in the following objects 
+    // => await Apartment.findByIdAndUpdate(id, {req.body);
+    // => await Apartment.create(id, {req.body);
+    console.log(req.body)
+
+  if (id) {
+    await Apartment.findByIdAndUpdate(id, {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      size: req.body.size,
+      mainPhoto: req.body.mainPhoto,
     });
 
-    res.send('Apartment added');
-}
+    res.send("Apartment updated");
+  } else {
+    await Apartment.create({
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      size: req.body.size,
+      mainPhoto: req.body.mainPhoto,
+    });
 
+    res.send("Apartment added");
+  }
+};
 
 module.exports = {
-    getNewApartmentForm,
-    postNewApartment
-}
+  getNewApartmentForm,
+  postNewApartment,
+  getEditApartmentForm,
+};
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 const adminControllers = require("../controllers/admin.js")
 
-router.get("/apartment/new-apartment", adminControllers.getNewApartmentForm )
-router.post("/apartment/new-apartment", adminControllers.postNewApartment )
+router.get("/apartment/new-apartment", adminControllers.getNewApartmentForm );
+router.post("/apartment/new-apartment", adminControllers.postNewApartment );
+router.get("/apartment/:idApartment/edit", adminControllers.getEditApartmentForm)
 
 module.exports = router;
