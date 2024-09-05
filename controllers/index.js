56f954389e21ff2d6164b5b20cc4546991d6eba8
kevmhughes <kevhughes24@hotmail.com>
2024-09-05 21:11:27 +0200

    Add basic apartment search functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const Apartment = require('../models/apartment.model.js');
+const Reservation = require("../models/reservation.model.js");
 
 const getApartments = async (req, res) => {
     const apartments = await Apartment.find();
@@ -11,7 +12,6 @@ const getApartments = async (req, res) => {
 }
 
 const getApartmentById = async (req, res) => {
-    console.log(req.params)
     const {idApartment } = req.params 
     const selectedApartment = await Apartment.findById(idApartment);
 
@@ -20,7 +20,41 @@ const getApartmentById = async (req, res) => {
     })
 }
 
+const searchApartments = async (req, res) => {
+    const { maxPrice } = req.query
+    const apartments = await Apartment.find({price: {$lte: maxPrice}})
+
+    if (apartments.length == 0) {
+        res.send("create an error message later")
+    }
+    res.render("home", {
+        apartments
+    })
+}
+
+const postNewReservation = async (req, res) => {
+
+   await Reservation.create(
+        {   
+        email: req.body.email,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        apartment: req.body.id
+        }
+   );
+
+   const checkReservation = await Reservation.find()
+   .populate('apartment')
+
+   console.log(checkReservation)
+
+   res.json(req.body);
+
+}
+
 module.exports = {
     getApartments,
-    getApartmentById
+    getApartmentById,
+    searchApartments,
+    postNewReservation
 }
