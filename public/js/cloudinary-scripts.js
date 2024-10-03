8b00b63f474b0714435fbffd678bbecb00820136
kevmhughes <kevhughes24@hotmail.com>
2024-10-03 21:25:32 +0200

    Add comments to clarify Cloudinary upload widget initialization.

--- public/js/cloudinary-scripts.js
+++ public/js/cloudinary-scripts.js
@@ -1,24 +1,42 @@
-      function initializeUploadWidget(buttonId, inputId) {
-        var widget = cloudinary.createUploadWidget({
-          cloudName: 'dsqhsfety',
-          uploadPreset: 'tribook_preset' // Configure this according to your settings
-        }, (error, result) => {
-          if (!error && result && result.event === "success") {
-            console.log('Done! Here is the image info: ', result.info);
-            console.log("the photo's url: ", result.info.url);
+/**
+ * Initializes the Cloudinary upload widget.
+ * @param {string} buttonId - The ID of the button that triggers the upload widget.
+ * @param {string} inputId - The ID of the input field where the image URL will be stored.
+ */
+function initializeUploadWidget(buttonId, inputId) {
+  // Create a new Cloudinary upload widget
+  var widget = cloudinary.createUploadWidget(
+    {
+      cloudName: "dsqhsfety", // Your Cloudinary cloud name
+      uploadPreset: "tribook_preset", // The preset for your upload configuration
+    },
+    (error, result) => {
+      // Handle the widget response
+      if (!error && result && result.event === "success") {
+        console.log("Done! Here is the image info: ", result.info); // Log the image information
+        console.log("the photo's url: ", result.info.url); // Log the URL of the uploaded photo
 
-            document.getElementById(inputId).value = result.info.url;
-          }
-        });
-
-        document.getElementById(buttonId).addEventListener("click", function () {
-          widget.open();
-        }, false);
+        // Set the value of the input field to the image URL
+        document.getElementById(inputId).value = result.info.url;
       }
+    }
+  );
 
-      document.addEventListener("DOMContentLoaded", function () {
-        initializeUploadWidget("upload_widget", "mainPhoto");
-        initializeUploadWidget("upload_widget_two", "photoTwo");
-        initializeUploadWidget("upload_widget_three", "photoThree");
-        initializeUploadWidget("upload_widget_four", "photoFour");
-      });
+  // Add click event listener to the specified button
+  document.getElementById(buttonId).addEventListener(
+    "click",
+    function () {
+      widget.open();
+    },
+    false
+  );
+}
+
+// Wait for the DOM to fully load before executing the following code
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize the upload widgets for different images
+  initializeUploadWidget("upload_widget", "mainPhoto");
+  initializeUploadWidget("upload_widget_two", "photoTwo");
+  initializeUploadWidget("upload_widget_three", "photoThree");
+  initializeUploadWidget("upload_widget_four", "photoFour");
+});
